2330ff9820e1c2e6c6b56f02b2633cffd00804ed
drajon13 <19345532@qq.com>
2019-06-13 09:16:24 +0800

    删除不要的框架

--- schie/schie-framework/src/main/java/org/schic/modules/server/service/ISysServerService.java
+++
@@ -1,7 +0,0 @@
-package org.schic.modules.server.service;
-
-import org.schic.common.persistence.InterfaceBaseService;
-import org.schic.modules.server.entity.SysServer;
-
-public interface ISysServerService extends InterfaceBaseService<SysServer> {
-}
--- schie/schie-framework/src/main/java/org/schic/modules/usercenter/service/ISysUserCenterService.java
+++
@@ -1,9 +0,0 @@
-package org.schic.modules.usercenter.service;
-
-import org.schic.common.persistence.InterfaceBaseService;
-import org.schic.modules.usercenter.entity.SysUserCenter;
-
-public interface ISysUserCenterService
-		extends
-			InterfaceBaseService<SysUserCenter> {
-}
